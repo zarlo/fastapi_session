235ba7c7383ea72e2ccd545b56a8f4fa7163407f
zarlo <zarlo5899@gmail.com>
2020-09-09 04:19:46 +1000

    update

--- flaskapi_session/__init__.py
+++ flaskapi_session/__init__.py
@@ -1,8 +1,10 @@
 from .session import Session
 from .session_interface import SessionInterface, BackendInterface
 from .session_manager import SessionManager
+from .session_filesystembackend import FileSystemBackend
 
 __all__ = [
+    FileSystemBackend,
     SessionInterface,
     BackendInterface,
     SessionManager,
